b4a45db735ef7de89789421e78d73bed19afd0f4
sasha <sasha432301@gmail.com>
2021-12-08 01:16:38 +0300

    last change

--- Homework__4/main.js
+++ Homework__4/main.js
@@ -1,20 +1,14 @@
 let str = prompt("Введите любую строку: ");
 
 function getVowels(strr) {
-    
-    strr = strr.toLowerCase();
+  strr = strr.toLowerCase();
   return function (x) {
     let counter = 0;
-    if (x) {
-      for (let i = 0; i < strr.length; i++) {
-        if (strr[i] === x) {
-          counter++;
-        }
-      }
+    let arr = strr.split("");
+    let arrVowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
+    if (arrVowels.includes(x)) {
+      counter = arr.reduce((sum, item) => (item === x ? ++sum : sum), 0);
     } else {
-      let arrVowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
-      let arr = strr.split("");
-      
       counter = arr.reduce((sum, item) => sum + arrVowels.includes(item), 0);
     }
     return counter;
@@ -23,10 +17,10 @@ function getVowels(strr) {
 let count = getVowels(str);
 let answer = confirm("Вы хотите найти конкретную гласную?");
 
-if(answer){
-    let vowel = prompt("Введите эту гласную:");
-    vowel = vowel.toLowerCase();
-    alert(`Количество гласных "${vowel}" в строке: ${count(vowel)}`);
-}else{
-    alert(`Количество ВСЕХ гласных в строке: ${count()}`);
+if (answer) {
+  let vowel = prompt("Введите эту гласную:");
+  vowel = vowel.toLowerCase();
+  alert(`Количество гласных "${vowel}" в строке: ${count(vowel)}`);
+} else {
+  alert(`Количество ВСЕХ гласных в строке: ${count()}`);
 }
